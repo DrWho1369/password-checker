3e2466806fef6c8825f1552c81ab658d385d3c32
Tom Baker <125075858+DrWho1369@users.noreply.github.com>
2024-12-09 15:05:28 +0000

    Add files via upload

---
+++ checkmypass.py
@@ -0,0 +1,51 @@
+import requests
+import hashlib
+import sys
+
+def request_api_data(query_char):
+    '''
+    Calls the API on the front 5 characters of the hashed password, returns the results
+    '''
+    url = "https://api.pwnedpasswords.com/range/" + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f"Error fetching: {res.status_code}, check the API and try again")
+    return res
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    '''
+    Splits the API response into hash and count, checks if "tail" is in the hash list, returns the related count
+    '''
+    hashes = (line.split(":") for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+def pwned_api_check(password):
+    '''
+    Hashing the password, splits it into a head and tail, passes this too the api call, then calls the count function on the API respons and the tail
+    '''
+    sha1password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+
+def main(args):
+    '''
+    Main function, calls password checks from system args
+    '''
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f"{password} was found {count} times.... you should change your password")
+        else:
+            print(f"{password} was NOT found. Carry on")
+    return "done!"
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
+    
